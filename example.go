c49c26663edca13a769b84c4b108b93920b903d1
bombjack73 <marc@walter.local>
2017-10-04 21:27:47 +0200

    dev

--- example.go
+++ example.go
@@ -19,11 +19,19 @@ func (g *mygame) FixedUpdate() {
 	fmt.Println("fixed updated")
 }
 
+/*
+func Update() {
+	fmt.Println("updated")
+}
+*/
 //https://github.com/oskca/gopherjs-canvas
 //import "github.com/veandco/go-sdl2/sdl"
 //todo allow passing argument and returning value
 //argc int, argv []string
 func main() {
+	//start from here for adapter
+	g := new(mygame)
+	(*mygame).Update(g)
 
 	//to add adapter to allow simple function without struct
 	gogame.Run(new(mygame))
