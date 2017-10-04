60cb8c0029a5ee82ed3ee1645329c1f053faea37
unknown <vandenbosch@BA_INFO33.CHBAH.ORG>
2017-10-04 17:26:54 +0200

    dev

--- example.go
+++ example.go
@@ -1,14 +1,30 @@
 package main
 
+import "fmt"
 import "github.com/gogame/gogame"
 
-/*type Example struct {
-}*/
+type mygame struct {
+	val int
+}
+
+func (g *mygame) Start() {
+	fmt.Println("started")
+}
+
+func (g *mygame) Update() {
+	fmt.Println("updated")
+}
+
+func (g *mygame) FixedUpdate() {
+	fmt.Println("fixed updated")
+}
 
 //https://github.com/oskca/gopherjs-canvas
 //import "github.com/veandco/go-sdl2/sdl"
 //todo allow passing argument and returning value
 //argc int, argv []string
 func main() {
-	gogame.Run()
+
+	//to add adapter to allow simple function without struct
+	gogame.Run(new(mygame))
 }
