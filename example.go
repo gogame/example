4dda44a688a6c6bd96f8f8adb458ab952abfa846
unknown <vandenbosch@BA_INFO33.CHBAH.ORG>
2017-10-04 16:08:13 +0200

    initial commit

---
+++ example.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gogame/gogame"
+
+/*type Example struct {
+}*/
+
+//https://github.com/oskca/gopherjs-canvas
+//import "github.com/veandco/go-sdl2/sdl"
+//todo allow passing argument and returning value
+//argc int, argv []string
+func main() {
+	gogame.Run()
+}
