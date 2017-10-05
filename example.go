c866b7aa7ee7098d70351d2f62ef53e26b5e2a0a
unknown <vandenbosch@BA_INFO33.CHBAH.ORG>
2017-10-05 15:29:09 +0200

    dev

--- example.go
+++ example.go
@@ -1,22 +1,33 @@
 package main
 
-import "fmt"
-import "github.com/gogame/gogame"
+import (
+	"fmt"
+
+	"github.com/gogame/gogame"
+)
 
 type mygame struct {
 	val int
 }
 
 func (g *mygame) Start() {
-	fmt.Println("started")
+	fmt.Println("started mg")
 }
 
 func (g *mygame) Update() {
-	fmt.Println("updated")
+	fmt.Println("updated mg")
 }
 
 func (g *mygame) FixedUpdate() {
 	fmt.Println("fixed updated")
+}
+
+func Start() {
+	fmt.Println("started free")
+}
+
+func Update() {
+	fmt.Println("updated free")
 }
 
 /*
@@ -30,9 +41,15 @@ func Update() {
 //argc int, argv []string
 func main() {
 	//start from here for adapter
-	g := new(mygame)
-	(*mygame).Update(g)
+	/*g := new(mygame)
+	(*mygame).Update(g)*/
 
 	//to add adapter to allow simple function without struct
-	gogame.Run(new(mygame))
+	//gogame.Run(new(mygame))
+	g := &gogame.Game{Update: &mygame{val: 1}}
+	g.Run()
+	//gogame.Run(&mygame{val: 1})
+	//gogame.Run(Start, Update)
+	//fmt.Println("------------------")
+	//gogame.Run(&mygame{val: 1})
 }
